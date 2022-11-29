b9216fdcfecf7b464ce4c16f84658eb05892a6ea
mhazizk <mhazizk@gmail.com>
2022-11-29 15:48:58 +0700

    Initial commit
    Generated by create-expo-app 1.1.4.

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View, StatusBar } from 'react-native';
-import BottomTab from './navigation/BottomTab';
-import 'react-native-gesture-handler';
-import RootStack from './navigation/RootStack';
-import { NavigationContainer } from '@react-navigation/native';
-import { GlobalStateProvider } from './modules/GlobalContext';
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
   return (
-    <GlobalStateProvider>
-      <NavigationContainer>
-        < RootStack />
-      </NavigationContainer>
-    </GlobalStateProvider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
@@ -23,6 +16,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: StatusBar.currentHeight
   },
 });
--- config/userAccount.js
+++
@@ -1,6 +0,0 @@
-const userAccount = {
-    username: '',
-    user_id: '',
-    logbook_id: ''
-}
-export default userAccount;
