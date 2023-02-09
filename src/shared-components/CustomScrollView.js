7652158761c897383129a165fb6ef5c70ae3a65d
mhazizk <mhazizk@gmail.com>
2023-02-10 00:31:09 +0700

    Update CustomScrollView.js

--- src/shared-components/CustomScrollView.js
+++ src/shared-components/CustomScrollView.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from "react-native";
+import { ScrollView } from "react-native";
 import { useGlobalTheme } from "../reducers/GlobalContext";
 
 const CustomScrollView = ({
