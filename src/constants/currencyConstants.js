7e1ef2a91e85199864d4d3bd1633c4eb14a37453
mhazizk <mhazizk@gmail.com>
2023-02-10 21:52:54 +0700

    Create currencyConstants.js

---
+++ src/constants/currencyConstants.js
@@ -0,0 +1,32 @@
+const CURRENCY_CONSTANTS = {
+  DEFAULT: { name: "IDR", symbol: "Rp", isoCode: "id" },
+  OPTIONS: [
+    {
+      name: "IDR",
+      symbol: "Rp",
+      isoCode: "id",
+    },
+    {
+      name: "USD",
+      symbol: "$",
+      isoCode: "us",
+    },
+    {
+      name: "EUR",
+      symbol: "€",
+      isoCode: "eu",
+    },
+    {
+      name: "GBP",
+      symbol: "£",
+      isoCode: "gb",
+    },
+    {
+      name: "JPY",
+      symbol: "¥",
+      isoCode: "jp",
+    },
+  ],
+};
+
+export default CURRENCY_CONSTANTS;
