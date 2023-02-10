7126fbd799f7cbce0d4b04695d6698917c5d1a65
mhazizk <mhazizk@gmail.com>
2023-02-10 21:52:47 +0700

    Create negativeCurrencySymbolConstants.js

---
+++ src/constants/negativeCurrencySymbolConstants.js
@@ -0,0 +1,9 @@
+const NEGATIVE_CURRENCY_SYMBOL_CONSTANTS = {
+  DEFAULT: { id: "-", name: "-" },
+  OPTIONS: [
+    { id: "-", name: "-123" },
+    { id: "()", name: "(123)" },
+  ],
+};
+
+export default NEGATIVE_CURRENCY_SYMBOL_CONSTANTS;
