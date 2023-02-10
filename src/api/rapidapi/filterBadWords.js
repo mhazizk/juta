433eb697ac115957d6cac48802ebc9a77e741c37
mhazizk <mhazizk@gmail.com>
2023-02-10 21:53:06 +0700

    Create filterBadWords.js

---
+++ src/api/rapidapi/filterBadWords.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const filterBadWords = async (text) => {
+  const options = {
+    method: "GET",
+    url: "https://community-purgomalum.p.rapidapi.com/json",
+    params: { text },
+    headers: {
+      "X-RapidAPI-Key": "e9ee1dd2e7mshd504c4cef4a0be6p1a0dadjsnd2b27b85c6ae",
+      "X-RapidAPI-Host": "community-purgomalum.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const { data } = await axios.request(options);
+    const { result } = data;
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default filterBadWords;
