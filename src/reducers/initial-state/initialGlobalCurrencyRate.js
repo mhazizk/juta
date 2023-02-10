82860b518abbd892433a21c9610acacb62ecb51c
mhazizk <mhazizk@gmail.com>
2023-02-10 21:51:41 +0700

    Create initialGlobalCurrencyRate.js

---
+++ src/reducers/initial-state/initialGlobalCurrencyRate.js
@@ -0,0 +1,17 @@
+const initialGlobalCurrencyRates = {
+  uid: null,
+  data: [
+    { name: "USD", rate: 1 },
+    { name: "IDR", rate: 15_000 },
+    { name: "EUR", rate: 0.93 },
+    { name: "GBP", rate: 0.83 },
+    { name: "JPY", rate: 132 },
+  ],
+  _timestamps: {
+    created_at: Date.now(),
+    created_by: null,
+    updated_at: Date.now(),
+    updated_by: null,
+  },
+};
+export default initialGlobalCurrencyRates;
