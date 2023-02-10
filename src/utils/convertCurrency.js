d496b39d7beeb5ce9d57e18486f04c42f9071e41
mhazizk <mhazizk@gmail.com>
2023-02-10 21:50:59 +0700

    Create convertCurrency.js

---
+++ src/utils/convertCurrency.js
@@ -0,0 +1,11 @@
+const convertCurrency = ({ amount, from, target, globalCurrencyRates }) => {
+  const { data } = globalCurrencyRates;
+  const dollarRate = data.find((c) => c.name === from).rate;
+  const targetRate = data.find((c) => c.name === target).rate;
+  const amountInDollar = amount / dollarRate;
+  const newAmount = amountInDollar * targetRate;
+
+  return newAmount;
+};
+
+export default convertCurrency;
