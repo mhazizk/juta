94f5b4d565cbbcfc78a3b0cd4b7e6bdcfd5d1fc6
mhazizk <mhazizk@gmail.com>
2023-02-11 18:31:42 +0700

    Update initialGlobalCurrencyRate.js

--- src/reducers/initial-state/initialGlobalCurrencyRate.js
+++ src/reducers/initial-state/initialGlobalCurrencyRate.js
@@ -1,11 +1,31 @@
 const initialGlobalCurrencyRates = {
   uid: null,
   data: [
-    { name: "USD", rate: 1 },
-    { name: "IDR", rate: 15_000 },
-    { name: "EUR", rate: 0.93 },
-    { name: "GBP", rate: 0.83 },
-    { name: "JPY", rate: 132 },
+    {
+      name: "USD",
+      isoCode: "us",
+      rate: 1,
+    },
+    {
+      name: "IDR",
+      isoCode: "id",
+      rate: 15_000,
+    },
+    {
+      name: "EUR",
+      isoCode: "eu",
+      rate: 0.93,
+    },
+    {
+      name: "GBP",
+      isoCode: "gb",
+      rate: 0.83,
+    },
+    {
+      name: "JPY",
+      isoCode: "jp",
+      rate: 132,
+    },
   ],
   _timestamps: {
     created_at: Date.now(),
