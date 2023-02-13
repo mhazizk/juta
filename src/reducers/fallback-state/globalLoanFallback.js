d656d4dc13a86c9e9861ae82f5a0d7273a755513
mhazizk <mhazizk@gmail.com>
2023-02-13 23:43:43 +0700

    Create globalLoanFallback.js

---
+++ src/reducers/fallback-state/globalLoanFallback.js
@@ -0,0 +1,20 @@
+const globalLoanFallback = {
+  uid: null,
+  loan_id: null,
+  contacts: [
+    {
+      name: "John Doe",
+      uid: "1234567890",
+      type: "individual",
+      transactions_id: [],
+    },
+  ],
+  _timestamps: {
+    created_at: Date.now(),
+    created_by: null,
+    updated_at: Date.now(),
+    updated_by: null,
+  },
+};
+
+export default globalLoanFallback;
