4288c95c36fdd0994bda1c54ea0a55491b02883d
mhazizk <mhazizk@gmail.com>
2023-02-13 23:43:25 +0700

    Create findTransactionsByIds.js

---
+++ src/utils/findTransactionsByIds.js
@@ -0,0 +1,24 @@
+const findTransactionsByIds = ({ transactionIds, groupSorted, callback }) => {
+  if (!transactionIds.length || !groupSorted) {
+    return callback([]);
+  }
+
+  if (!!transactionIds.length && groupSorted) {
+    const foundTransactions = [];
+    transactionIds.forEach((id) => {
+      groupSorted.forEach((logbook) => {
+        logbook.transactions.forEach((section) => {
+          section.data.forEach((transaction) => {
+            if (transaction.transaction_id === id) {
+              foundTransactions.push(transaction);
+            }
+          });
+        });
+      });
+    });
+    
+    return callback(foundTransactions);
+  }
+};
+
+export default findTransactionsByIds;
