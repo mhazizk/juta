d33d845531f036f89e9ea36f9903e1f35eceb068
mhazizk <mhazizk@gmail.com>
2023-02-15 22:25:48 +0700

    Update MyLoansWidget.js

--- src/features/dashboard/components/MyLoansWidget.js
+++ src/features/dashboard/components/MyLoansWidget.js
@@ -13,8 +13,16 @@ import {
 import { useIsFocused } from "@react-navigation/native";
 import screenList from "../../../navigations/ScreenList";
 import Loading from "../../../components/Loading";
+import TextTicker from "react-native-text-ticker";
 
-const MyLoansWidget = ({ onPress }) => {
+const MyLoansWidget = ({
+  height = 150,
+  marginRight = 0,
+  marginHorizontal = 0,
+  marginVertical = 0,
+  width = Dimensions.get("window").width / 2 - 24,
+  onPress,
+}) => {
   const { globalTheme } = useGlobalTheme();
   const { globalLoan } = useGlobalLoan();
   const { appSettings } = useGlobalAppSettings();
@@ -48,7 +56,7 @@ const MyLoansWidget = ({ onPress }) => {
   }, [isFocused]);
 
   useEffect(() => {
-    console.log({ transactionsDetails });
+    // console.log({ transactionsDetails });
   }, [transactionsDetails]);
 
   const getTotalAmount = (transactionDetails) => {
@@ -64,18 +72,67 @@ const MyLoansWidget = ({ onPress }) => {
     return totalAmount.reduce((a, b) => a + b, 0);
   };
 
+  const getNextPayment = ({
+    getNextAmount = false,
+    getNextDate = false,
+    globalLoan,
+    groupSorted,
+  }) => {
+    // get all the transactions that are not paid
+    const contactsNotYetPaid = globalLoan.contacts.filter((contact) => {
+      return contact.is_paid === false;
+    });
+
+    // get neareast payment due date
+    const nearestPaymentDueInMillis = contactsNotYetPaid.sort((a, b) => {
+      return a.payment_due_date - b.payment_due_date;
+    });
+    //   get nearest payment due date in date
+    const nearestPaymentDueInDate = new Date(
+      nearestPaymentDueInMillis[0].payment_due_date
+    ).getDate();
+
+    // get transactions id from contacts that have the nearest payment due date
+    const transactionIdsToFind = [];
+    contactsNotYetPaid.forEach((contact) => {
+      if (
+        new Date(contact.payment_due_date).getDate() === nearestPaymentDueInDate
+      ) {
+        transactionIdsToFind.push(...contact.transactions_id);
+      }
+    });
+
+    switch (true) {
+      case getNextAmount:
+        // get transactions details from transaction ids
+        return utils.findTransactionsByIds({
+          transactionIds: transactionIdsToFind,
+          groupSorted: groupSorted,
+          callback: (transactions) => {
+            return getTotalAmount(transactions);
+          },
+        });
+      case getNextDate:
+        return nearestPaymentDueInDate - new Date().getDate();
+
+      default:
+        break;
+    }
+  };
   return (
     <>
       {isLoading && (
         <View
           style={{
             // flex: 1,
-            height: 150,
-            width: Dimensions.get("window").width / 2 - 24,
-            marginRight: 8,
+            height: height,
+            width: width,
+            marginRight: marginRight,
+            marginHorizontal,
+            marginVertical,
             alignItems: "center",
             justifyContent: "center",
-            backgroundColor: globalTheme.colors.secondary,
+            backgroundColor: globalTheme.widgets.myLoans.cardBackgroundColor,
             marginBottom: 16,
             borderRadius: 16,
           }}
@@ -88,10 +145,13 @@ const MyLoansWidget = ({ onPress }) => {
           <TouchableOpacity onPress={() => onPress()}>
             <View
               style={{
-                height: 150,
-                width: Dimensions.get("window").width / 2 - 24,
-                backgroundColor: "#90CEFF",
-                marginRight: 8,
+                height: height,
+                width: width,
+                backgroundColor:
+                  globalTheme.widgets.myLoans.cardBackgroundColor,
+                marginRight: marginRight,
+                marginHorizontal,
+                marginVertical,
                 borderRadius: 16,
                 alignItems: "flex-start",
                 overflow: "hidden",
@@ -104,30 +164,40 @@ const MyLoansWidget = ({ onPress }) => {
                     fontWeight: "bold",
                     fontSize: 18,
                     padding: 16,
-                    color: globalTheme.colors.headerText,
+                    color: globalTheme.widgets.myLoans.cardTextColor,
                   }}
                 />
               )}
-
               {getTotalAmount(transactionsDetails) !== 0 && (
                 <>
                   <View
                     style={{
                       height: "100%",
                       width: "100%",
-                      alignItems: "center",
+                      alignItems: "flex-start",
+                      padding: 16,
                       justifyContent: "center",
                     }}
                   >
                     <TextPrimary
-                      label="Active Debt/Loan"
+                      label="My Loans"
                       style={{
                         fontWeight: "bold",
                         fontSize: 18,
-                        padding: 4,
-                        color: globalTheme.colors.headerText,
+                        flex: 1,
+                        color: globalTheme.widgets.myLoans.cardTextColor,
                       }}
                     />
+
+                    <TextPrimary
+                      label="Next payment"
+                      style={{
+                        fontWeight: "bold",
+                        fontSize: 18,
+                        color: globalTheme.widgets.myLoans.cardTextColor,
+                      }}
+                    />
+                    {/* // TAG : Text Ticker */}
                     {/* // TAG : Main currency */}
                     <View
                       style={{
@@ -143,17 +213,23 @@ const MyLoansWidget = ({ onPress }) => {
                         style={{
                           fontSize: 24,
                           paddingRight: 4,
-                          color: globalTheme.colors.cardText,
+                          color: globalTheme.widgets.myLoans.cardTextColor,
                         }}
                       />
                       <TextPrimary
                         style={{
                           fontSize: 32,
                           fontWeight: "bold",
-                          color: globalTheme.colors.cardText,
+                          color: globalTheme.widgets.myLoans.cardTextColor,
                         }}
                         label={utils.getFormattedNumber({
-                          value: getTotalAmount(transactionsDetails),
+                          value: Math.abs(
+                            getNextPayment({
+                              getNextAmount: true,
+                              globalLoan,
+                              groupSorted: sortedTransactions.groupSorted,
+                            })
+                          ),
                           currencyIsoCode:
                             appSettings.logbookSettings.defaultCurrency.isoCode,
                           negativeSymbol:
@@ -180,18 +256,24 @@ const MyLoansWidget = ({ onPress }) => {
                           }
                           style={{
                             paddingRight: 4,
-                            color: globalTheme.colors.cardText,
+                            color: globalTheme.widgets.myLoans.cardTextColor,
                           }}
                         />
                         <TextPrimary
                           style={{
                             fontSize: 24,
                             fontWeight: "bold",
-                            color: globalTheme.colors.cardText,
+                            color: globalTheme.widgets.myLoans.cardTextColor,
                           }}
                           label={utils.getFormattedNumber({
                             value: utils.convertCurrency({
-                              amount: getTotalAmount(transactionsDetails),
+                              amount: Math.abs(
+                                getNextPayment({
+                                  getNextAmount: true,
+                                  globalLoan,
+                                  groupSorted: sortedTransactions.groupSorted,
+                                })
+                              ),
                               from: appSettings.logbookSettings.defaultCurrency
                                 .name,
                               target:
@@ -209,13 +291,28 @@ const MyLoansWidget = ({ onPress }) => {
                         />
                       </View>
                     )}
+                    <TextPrimary
+                      label={`in ${getNextPayment({
+                        getNextDate: true,
+                        globalLoan,
+                        groupSorted: sortedTransactions.groupSorted,
+                      })} days`}
+                      style={{
+                        fontSize: 18,
+                        textAlign: "center",
+                        fontWeight: "bold",
+                        color: globalTheme.widgets.myLoans.cardTextColor,
+                      }}
+                    />
                   </View>
                 </>
               )}
-
               <IonIcons
                 name="cash"
-                color="#48ADFF"
+                color={utils.hexToRgb({
+                  hex: globalTheme.widgets.myLoans.cardTextColor,
+                  opacity: 0.3,
+                })}
                 size={100}
                 style={{
                   zIndex: -1,
