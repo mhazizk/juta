8db515267e172b37b0143a97deb1253a0133f2ca
mhazizk <mhazizk@gmail.com>
2023-02-15 14:55:46 +0700

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 export { default as DailyLimit } from "./DailyLimit";
 export { default as FindById } from "./FindById";
 export { default as FormatCurrency } from "./FormatCurrency";
-export { default as HexToRgb } from "./HexToRGB";
+export { default as hexToRgb } from "./hexToRGB";
 export { default as RelativeDate } from "./RelativeDate";
 export { default as GetSpentList } from "./GetSpentList";
 export { default as FindTransactionsToPlot } from "./FindTransactionsToPlot";
