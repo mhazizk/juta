b08d823e3d315969037512e5d17e1e7b469392e3
mhazizk <mhazizk@gmail.com>
2023-02-17 23:46:57 +0700

    Update getTotalAmountAndConvertToDefaultCurrency.js

--- src/utils/getTotalAmountAndConvertToDefaultCurrency.js
+++ src/utils/getTotalAmountAndConvertToDefaultCurrency.js
@@ -1,7 +1,18 @@
 import convertCurrency from "./convertCurrency";
 import FindById from "./FindById";
 
+/**
+ * Get total amount and convert to default currency
+ *
+ * @param invertResult  - if true, expense will be marked as (+) and income will be marked as (-)
+ * @param transactions  - array of transaction details
+ * @param logbooks - array of logbooks
+ * @param targetCurrencyName - target currency name
+ * @param globalCurrencyRates - global currency rates
+ * @returns
+ */
 const getTotalAmountAndConvertToDefaultCurrency = ({
+  invertResult = false,
   transactions,
   logbooks,
   targetCurrencyName,
@@ -14,10 +25,24 @@ const getTotalAmountAndConvertToDefaultCurrency = ({
       logbooks: logbooks,
     }).logbook_currency.name;
     let amount = 0;
-    if (transaction.details.in_out === "expense") {
-      amount = +transaction.details.amount;
-    } else {
-      amount = -transaction.details.amount;
+    switch (invertResult) {
+      case true:
+        if (transaction.details.in_out === "expense") {
+          amount = +transaction.details.amount;
+        } else {
+          amount = -transaction.details.amount;
+        }
+        break;
+      case false:
+        if (transaction.details.in_out === "expense") {
+          amount = -transaction.details.amount;
+        } else {
+          amount = +transaction.details.amount;
+        }
+        break;
+
+      default:
+        break;
     }
     const convertedAmount = convertCurrency({
       amount,
