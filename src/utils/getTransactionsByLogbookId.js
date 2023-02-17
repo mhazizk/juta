a539cf4b0a96b8f83c7498ad6e6430f447abecca
mhazizk <mhazizk@gmail.com>
2023-02-17 23:46:50 +0700

    Create getTransactionsByLogbookId.js

---
+++ src/utils/getTransactionsByLogbookId.js
@@ -0,0 +1,22 @@
+/**
+ * Get transactions by logbook id
+ *
+ * @param groupSorted - groupSorted object
+ * @param logbookId - logbook id
+ * @returns
+ */
+const getTransactionsByLogbookId = ({ groupSorted, logbookId }) => {
+  let array = [];
+  groupSorted.forEach((logbook) => {
+    logbook.transactions.forEach((section) => {
+      section.data.forEach((transaction) => {
+        if (transaction.logbook_id === logbookId) {
+          array.push(transaction);
+        }
+      });
+    });
+  });
+  return array;
+};
+
+export default getTransactionsByLogbookId;
