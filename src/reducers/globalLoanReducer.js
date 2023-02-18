df1cb3831b3643028b95358841eb0d9091de6764
mhazizk <mhazizk@gmail.com>
2023-02-18 18:34:49 +0700

    Update globalLoanReducer.js

--- src/reducers/globalLoanReducer.js
+++ src/reducers/globalLoanReducer.js
@@ -1,6 +1,11 @@
 import REDUCER_ACTIONS from "./reducer.action";
 
 const globalLoanReducer = (state, action) => {
+  const isPaid = action.payload?.isPaid;
+  const willPrevLoanContactBePaid = action.payload?.willPrevLoanContactBePaid;
+  const willTargetLoanContactBePaid =
+    action.payload?.willTargetLoanContactBePaid;
+  const patchTransaction = action.payload?.patchTransaction;
   // Loan contact
   const insertLoanContact = action.payload?.insertLoanContact;
   const patchLoanContact = action.payload?.patchLoanContact;
@@ -111,6 +116,7 @@ const globalLoanReducer = (state, action) => {
             }),
             {
               ...foundExistingContact,
+              is_paid: isPaid,
               transactions_id: [
                 ...foundExistingContact.transactions_id,
                 insertLoanTransaction,
@@ -125,14 +131,132 @@ const globalLoanReducer = (state, action) => {
         return state;
       }
 
+    case REDUCER_ACTIONS.LOAN.PATCH_ONE_TRANSACTION_TO_LOAN_CONTACT:
+      //  check timestamp
+      if (newUpdatedAtTimestampFromLocal > existingUpdatedAtTimestamp) {
+        //   find the existing contact by transaction id
+        const previousContact = state.contacts.find((contact) => {
+          return contact.transactions_id.includes(
+            patchTransaction.transaction_id
+          );
+        });
+        const targetContact = state.contacts.find((contact) => {
+          return contact.contact_uid === targetLoanContactUid;
+        });
+
+        const isContactSame =
+          previousContact?.contact_uid === targetContact?.contact_uid;
+        console.log("line 149");
+        let newPrevContact;
+        if (previousContact) {
+          newPrevContact = {
+            ...previousContact,
+            is_paid: willPrevLoanContactBePaid,
+            transactions_id: [
+              ...previousContact.transactions_id.filter((id) => {
+                return id !== patchTransaction.transaction_id;
+              }),
+            ],
+          };
+        }
+        let newTargetContact;
+        if (targetContact) {
+          newTargetContact = {
+            ...targetContact,
+            is_paid: willTargetLoanContactBePaid,
+            transactions_id: [
+              ...targetContact.transactions_id,
+              patchTransaction.transaction_id,
+            ],
+          };
+        }
+        console.log(JSON.stringify({ patchTransaction }, null, 2));
+        console.log(JSON.stringify({ previousContact }, null, 2));
+        console.log(JSON.stringify({ targetLoanContactUid }, null, 2));
+        console.log(JSON.stringify({ targetContact }, null, 2));
+
+        switch (true) {
+          case isContactSame:
+            console.log("line 153");
+            return {
+              ...state,
+              contacts: [
+                ...state.contacts.filter((contact) => {
+                  return contact.contact_uid !== targetContact.contact_uid;
+                }),
+                {
+                  ...targetContact,
+                  is_paid: isPaid,
+                },
+              ].sort((a, b) => {
+                return a.contact_name - b.contact_name;
+              }),
+              _timestamps: { ...newLocalTimestamp },
+            };
+          case !isContactSame && !previousContact && !!targetContact:
+            console.log("line 178");
+
+            return {
+              ...state,
+              contacts: [
+                ...state.contacts.filter((contact) => {
+                  return contact.contact_uid !== newTargetContact.contact_uid;
+                }),
+                newTargetContact,
+              ].sort((a, b) => {
+                return a.contact_name - b.contact_name;
+              }),
+              _timestamps: { ...newLocalTimestamp },
+            };
+
+          case !isContactSame && !!previousContact && !targetContact:
+            console.log("line 201");
+            return {
+              ...state,
+              contacts: [
+                ...state.contacts.filter((contact) => {
+                  return contact.contact_uid !== newPrevContact.contact_uid;
+                }),
+                newPrevContact,
+              ].sort((a, b) => {
+                return a.contact_name - b.contact_name;
+              }),
+              _timestamps: { ...newLocalTimestamp },
+            };
+
+          case !isContactSame && !!previousContact && !!targetContact:
+            console.log("line 216");
+
+            return {
+              ...state,
+              contacts: [
+                ...state.contacts.filter((contact) => {
+                  return (
+                    contact.contact_uid !== newPrevContact.contact_uid &&
+                    contact.contact_uid !== newTargetContact.contact_uid
+                  );
+                }),
+                newPrevContact,
+                newTargetContact,
+              ].sort((a, b) => {
+                return a.contact_name - b.contact_name;
+              }),
+              _timestamps: { ...newLocalTimestamp },
+            };
+        }
+      } else {
+        console.log("line 233");
+        return state;
+      }
+      console.log("line 236");
+      return state;
+
     case REDUCER_ACTIONS.LOAN.DELETE_ONE_TRANSACTION_FROM_LOAN_CONTACT:
       //  check timestamp
       if (newUpdatedAtTimestampFromLocal > existingUpdatedAtTimestamp) {
         //   find the existing contact by transaction id
         const foundExistingContact = state.contacts.find((contact) => {
-          return contact.transactions_id.includes(deleteLoanTransaction)
-            ? contact
-            : null;
+          return contact.transactions_id.includes(deleteLoanTransaction);
         });
 
         // remove the transaction from the found contact
@@ -151,6 +275,7 @@ const globalLoanReducer = (state, action) => {
             }),
             {
               ...foundExistingContact,
+              is_paid: isPaid,
               transactions_id: [...filterOutExistingTransaction],
             },
           ].sort((a, b) => {
