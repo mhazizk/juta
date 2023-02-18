fdb8f134e6e9fe423374aefa10b5d420db16594e
mhazizk <mhazizk@gmail.com>
2023-02-19 01:57:34 +0700

    Create handleUserUpdateProfile.js

---
+++ src/utils/handleUserUpdateProfile.js
@@ -0,0 +1,23 @@
+import { updateProfile } from "firebase/auth/react-native";
+import { Alert } from "react-native";
+import auth from "../api/firebase/auth";
+import createNewUser from "../api/firebase/createNewUser";
+
+const handleUserUpdateProfile = async ({
+  displayName = null,
+  photoURL = null,
+}) => {
+  try {
+    return await updateProfile(auth.currentUser, { displayName, photoURL });
+  } catch (error) {
+    switch (true) {
+      case error.message.includes("email-already-in-use"):
+        return Alert.alert("Account", "Email address is already registered");
+
+      default:
+        return Alert.alert("Account", error.message.replace("Firebase: ", ""));
+    }
+  }
+};
+
+export default handleUserUpdateProfile;
