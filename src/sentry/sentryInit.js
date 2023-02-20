a4656ce1b65bddbd65c2e69b054cd93baf95eb79
mhazizk <mhazizk@gmail.com>
2023-02-21 01:21:02 +0700

    Update sentryInit.js

--- src/sentry/sentryInit.js
+++ src/sentry/sentryInit.js
@@ -3,7 +3,7 @@ import env from "../config/env";
 
 const sentryInit = () => {
   return Sentry.init({
-    dsn: env.sentryConfig.dsn,
+    dsn: env.SENTRY_CONFIG.SENTRY_DSN,
     enableInExpoDevelopment: true,
     // enableNative: true,
     debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
