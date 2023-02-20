94215c54dd4938419512c66d16bc23ee876e83c0
mhazizk <mhazizk@gmail.com>
2023-02-21 01:20:01 +0700

    Update getRevenueCatOfferings.js

--- src/api/revenue-cat/getRevenueCatOfferings.js
+++ src/api/revenue-cat/getRevenueCatOfferings.js
@@ -1,12 +1,14 @@
 import { Platform } from "react-native";
 import Purchases from "react-native-purchases";
 import env from "../../config/env";
+import SECRET_KEYS from "../../constants/secretManager";
+import getSecretFromCloudFunctions from "../firebase/getSecretFromCloudFunctions";
 
 const getRevenueCatOfferings = async () => {
   const apiKey =
     Platform.OS == "android"
-      ? env.revenueCat.googleApiKey
-      : env.revenueCat.appleApiKey;
+      ? await getSecretFromCloudFunctions(SECRET_KEYS.REVENUE_CAT_GOOGLE_API_KEY)
+      : null; // Apple API key for RevenueCat
   return Promise.all([
     Purchases.configure({ apiKey: apiKey }),
     Purchases.setDebugLogsEnabled(true),
