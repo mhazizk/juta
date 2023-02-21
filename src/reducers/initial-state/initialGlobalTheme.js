b2c250bec908bb4aed56d5fe6cb38191c1049748
mhazizk <mhazizk@gmail.com>
2023-02-22 00:01:25 +0700

    Update initialGlobalTheme.js

--- src/reducers/initial-state/initialGlobalTheme.js
+++ src/reducers/initial-state/initialGlobalTheme.js
@@ -1,6 +1,6 @@
 import { darkMonoTheme } from "../../assets/themes/darkMonoTheme/darkMonoTheme";
 import { lightMonoTheme } from "../../assets/themes/lightMonoTheme/lightMonoTheme";
 
-const initialGlobalTheme = lightMonoTheme;
+const initialGlobalTheme = darkMonoTheme;
 
 export default initialGlobalTheme;
