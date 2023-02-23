204d7b66d4bb9d003c87519e5310f511dc1ddf4d
mhazizk <mhazizk@gmail.com>
2023-02-23 18:33:42 +0700

    Update LogoutScreen.js

--- src/features/auth/screens/LogoutScreen.js
+++ src/features/auth/screens/LogoutScreen.js
@@ -10,7 +10,16 @@ import screenList from "../../../navigations/ScreenList";
 import appSettingsFallback from "../../../reducers/fallback-state/appSettingsFallback";
 import {
   useGlobalAppSettings,
+  useGlobalBadgeCounter,
+  useGlobalBudgets,
+  useGlobalCategories,
+  useGlobalCurrencyRates,
+  useGlobalLoan,
+  useGlobalLogbooks,
+  useGlobalRepeatedTransactions,
+  useGlobalSortedTransactions,
   useGlobalTheme,
+  useGlobalTransactions,
   useGlobalUserAccount,
 } from "../../../reducers/GlobalContext";
 import REDUCER_ACTIONS from "../../../reducers/reducer.action";
@@ -20,6 +29,17 @@ const LogoutScreen = ({ navigation }) => {
   const { appSettings, dispatchAppSettings } = useGlobalAppSettings();
   const { globalTheme, dispatchGlobalTheme } = useGlobalTheme();
   const { userAccount, dispatchUserAccount } = useGlobalUserAccount();
+  const { globalLoan, dispatchGlobalLoan } = useGlobalLoan();
+  const { globalCurrencyRates, dispatchGlobalCurrencyRates } =
+    useGlobalCurrencyRates();
+  const { logbooks, dispatchLogbooks } = useGlobalLogbooks();
+  const { sortedTransactions, dispatchSortedTransactions } =
+    useGlobalSortedTransactions();
+  const { categories, dispatchCategories } = useGlobalCategories();
+  const { budgets, dispatchBudgets } = useGlobalBudgets();
+  const { repeatedTransactions, dispatchRepeatedTransactions } =
+    useGlobalRepeatedTransactions();
+  const { badgeCounter, dispatchBadgeCounter } = useGlobalBadgeCounter();
 
   useEffect(() => {
     const {
@@ -36,6 +56,36 @@ const LogoutScreen = ({ navigation }) => {
     } = useFirestoreSubscriptions({
       uid: userAccount.uid,
       unsubscribeAll: true,
+
+      appSettings,
+      dispatchAppSettings,
+
+      userAccount,
+      dispatchUserAccount,
+
+      logbooks,
+      dispatchLogbooks,
+
+      sortedTransactions,
+      dispatchSortedTransactions,
+
+      categories,
+      dispatchCategories,
+
+      repeatedTransactions,
+      dispatchRepeatedTransactions,
+
+      budgets,
+      dispatchBudgets,
+
+      badgeCounter,
+      dispatchBadgeCounter,
+
+      globalCurrencyRates,
+      dispatchGlobalCurrencyRates,
+
+      globalLoan,
+      dispatchGlobalLoan,
     });
 
     // unsubscribe from firestore subscriptions
