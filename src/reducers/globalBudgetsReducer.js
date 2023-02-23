30f701ee8ad573539bc01cc78a703400326e80e2
mhazizk <mhazizk@gmail.com>
2023-02-23 13:17:42 +0700

    Update globalBudgetsReducer.js

--- src/reducers/globalBudgetsReducer.js
+++ src/reducers/globalBudgetsReducer.js
@@ -1,7 +1,11 @@
 import REDUCER_ACTIONS from "./reducer.action";
 
 const globalBudgetsReducer = (state, action) => {
-  let reducerUpdatedAt;
+  let reducerUpdatedAt = action.payload.reducerUpdatedAt;
+  let insertBudget = action.payload.insertBudget;
+  let patchBudget = action.payload.patchBudget;
+  let deleteBudget = action.payload.deleteBudget;
+
   switch (action.type) {
     case REDUCER_ACTIONS.BUDGETS.FORCE_SET:
       return action.payload;
@@ -10,95 +14,84 @@ const globalBudgetsReducer = (state, action) => {
       return {
         reducerUpdatedAt: Date.now(),
         budgets: [action.payload],
-        // budgetPatchCounter: 0,
-        // budgetInsertCounter: 0,
-        // budgetDeleteCounter: 0,
       };
 
     case REDUCER_ACTIONS.BUDGETS.INSERT:
-      reducerUpdatedAt = action.payload.reducerUpdatedAt;
-      let sortInsertedBudgets = [
-        ...state.budgets,
-        action.payload.insertBudget,
-      ].sort((a, b) => {
-        if (a.budget_name < b.budget_name) {
-          return -1;
+      let sortInsertedBudgets = [...state.budgets, insertBudget].sort(
+        (a, b) => {
+          if (a.budget_name < b.budget_name) {
+            return -1;
+          }
+          if (a.budget_name > b.budget_name) {
+            return 1;
+          }
+          return 0;
         }
-        if (a.budget_name > b.budget_name) {
-          return 1;
-        }
-        return 0;
-      });
+      );
 
       return {
         ...state,
         budgets: [...sortInsertedBudgets],
         reducerUpdatedAt,
-        // budgetInsertCounter: state.budgetInsertCounter + 1,
       };
 
     case REDUCER_ACTIONS.BUDGETS.PATCH:
-      reducerUpdatedAt = action.payload.reducerUpdatedAt;
-      let patchBudget = action.payload.patchBudget;
+      if (reducerUpdatedAt > state.reducerUpdatedAt) {
+        let foundOtherBudgets = state.budgets.filter(
+          (budget) => budget?.budget_id !== patchBudget.budget_id
+        );
 
-      let foundBudget = state.budgets.filter(
-        (budget) => budget.budget_id === patchBudget.budget_id
-      );
+        let sortBudgets = [...foundOtherBudgets, patchBudget].sort((a, b) => {
+          if (a.budget_name < b.budget_name) {
+            return -1;
+          }
+          if (a.budget_name > b.budget_name) {
+            return 1;
+          }
+          return 0;
+        });
 
-      let foundOtherBudgets = state.budgets.filter(
-        (budget) => budget.budget_id !== patchBudget.budget_id
-      );
-
-      let sortBudgets = [...foundOtherBudgets, patchBudget].sort((a, b) => {
-        if (a.budget_name < b.budget_name) {
-          return -1;
-        }
-        if (a.budget_name > b.budget_name) {
-          return 1;
-        }
-        return 0;
-      });
-
-      return {
-        ...state,
-        budgets: [...sortBudgets],
-        reducerUpdatedAt,
-        // budgetPatchCounter: state.budgetPatchCounter + 1,
-      };
-
-    case REDUCER_ACTIONS.BUDGETS.DELETE_ONE:
-      reducerUpdatedAt = action.payload.reducerUpdatedAt;
-      let deleteBudget = action.payload.deleteBudget;
-
-      let foundOtherBudget = state.budgets.filter(
-        (budget) => budget.budget_id !== deleteBudget.budget_id
-      );
-
-      if (!foundOtherBudget.length) {
         return {
           ...state,
-          budgets: [],
+          budgets: [...sortBudgets],
           reducerUpdatedAt,
-          // budgetDeleteCounter: state.budgetDeleteCounter + 1,
         };
+      } else {
+        return state;
       }
 
-      let sortOtherBudgets = foundOtherBudget.sort((a, b) => {
-        if (a.budget_name < b.budget_name) {
-          return -1;
-        }
-        if (a.budget_name > b.budget_name) {
-          return 1;
-        }
-        return 0;
-      });
+    case REDUCER_ACTIONS.BUDGETS.DELETE_ONE:
+      if (reducerUpdatedAt > state.reducerUpdatedAt) {
+        let foundOtherBudget = state.budgets.filter(
+          (budget) => budget.budget_id !== deleteBudget.budget_id
+        );
 
-      return {
-        ...state,
-        budgets: [...sortOtherBudgets],
-        reducerUpdatedAt,
-        // budgetDeleteCounter: state.budgetDeleteCounter + 1,
-      };
+        if (!foundOtherBudget.length) {
+          return {
+            ...state,
+            budgets: [],
+            reducerUpdatedAt,
+          };
+        }
+
+        let sortOtherBudgets = foundOtherBudget.sort((a, b) => {
+          if (a.budget_name < b.budget_name) {
+            return -1;
+          }
+          if (a.budget_name > b.budget_name) {
+            return 1;
+          }
+          return 0;
+        });
+
+        return {
+          ...state,
+          budgets: [...sortOtherBudgets],
+          reducerUpdatedAt,
+        };
+      } else {
+        return state;
+      }
 
     default:
       return state;
