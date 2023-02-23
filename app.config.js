35f4fe559c02c1636b5549d4af7a310da024d591
mhazizk <mhazizk@gmail.com>
2023-02-23 18:37:35 +0700

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,8 +1,8 @@
 module.exports = ({ config }) => {
   const name = "Juta";
 
-  const appVersion = "0.7.3-alpha";
-  const androidVersionCode = 17;
+  const appVersion = "0.7.5-alpha";
+  const androidVersionCode = 18;
   const iosBuildNumber = 1;
 
   return {
