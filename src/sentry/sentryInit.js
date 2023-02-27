b06c896708784f2485ab7e11ede42169124966c9
mhazizk <mhazizk@gmail.com>
2023-02-27 09:04:28 +0700

    Update sentryInit.js

--- src/sentry/sentryInit.js
+++ src/sentry/sentryInit.js
@@ -1,12 +1,14 @@
 import * as Sentry from "sentry-expo";
+// import * as Sentry from "@sentry/react-native";
 import env from "../config/env";
 
 const sentryInit = () => {
   return Sentry.init({
     dsn: env.SENTRY_CONFIG.SENTRY_DSN,
-    enableInExpoDevelopment: true,
-    // enableNative: true,
     debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+    enableInExpoDevelopment: true,
+    attachScreenshot: true,
+    enableAutoSessionTracking: true,
   });
 };
 export default sentryInit;
