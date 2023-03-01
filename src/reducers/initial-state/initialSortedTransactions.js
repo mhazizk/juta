29ab00b8bbd0ea4c316c37c6e5f1a3322ecbcae4
mhazizk <mhazizk@gmail.com>
2023-03-01 14:37:21 +0700

    Update initialSortedTransactions.js

--- src/reducers/initial-state/initialSortedTransactions.js
+++ src/reducers/initial-state/initialSortedTransactions.js
@@ -2,12 +2,6 @@ const initialSortedTransactions = {
   reducerUpdatedAt: Date.now(),
   logbookToOpen: null,
   groupSorted: [],
-  // sortedTransactionsInitCounter: 0,
-  // sortedTransactionsInsertCounter: 0,
-  // sortedTransactionsPatchCounter: 0,
-  // sortedTransactionsDeleteCounter: 0,
-  // sortedLogbookInsertCounter: 0,
-  // sortedLogbookDeleteCounter: 0,
 };
 
 export default initialSortedTransactions;
