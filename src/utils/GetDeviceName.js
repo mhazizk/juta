923c3be2c160fcda4b58d6be98a5bf2c7837c28a
mhazizk <mhazizk@gmail.com>
2023-03-01 18:27:09 +0700

    Update GetDeviceName.js

--- src/utils/GetDeviceName.js
+++ src/utils/GetDeviceName.js
@@ -1,6 +1,6 @@
 import * as Device from "expo-device";
 import * as Application from "expo-application";
-
+import { Platform } from "react-native";
 const getDeviceName = () => {
   return Device.deviceName;
 };
