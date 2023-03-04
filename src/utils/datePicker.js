3a2b49a026e6827dbad64d7ab96c1c1c9f139f32
mhazizk <mhazizk@gmail.com>
2023-03-04 20:55:15 +0700

    Update datePicker.js

--- src/utils/datePicker.js
+++ src/utils/datePicker.js
@@ -1,6 +1,12 @@
-import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+import RNDateTimePicker, {
+  DateTimePickerAndroid,
+} from "@react-native-community/datetimepicker";
+import { Platform } from "react-native";
+import { getCalendars } from "expo-localization";
 
-// Date Picker
+const is24Hours = getCalendars()[0]?.uses24hourClock;
+
+// Date Picker Show Mode
 const showMode = ({ currentMode, minimumDate, selectedDate, callback }) => {
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate;
@@ -14,7 +20,7 @@ const showMode = ({ currentMode, minimumDate, selectedDate, callback }) => {
       value: selectedDate,
       onChange: onChange,
       mode: currentMode,
-      is24Hour: true,
+      is24Hour: is24Hours,
       minimumDate: minimumDate,
     });
   }
@@ -24,46 +30,62 @@ const showMode = ({ currentMode, minimumDate, selectedDate, callback }) => {
       value: selectedDate,
       onChange: onChange,
       mode: currentMode,
-      is24Hour: true,
+      is24Hour: is24Hours,
       minimumDate: minimumDate,
     });
   }
 };
 
-// Date Picker
+/**
+ * Date Picker
+ *
+ * @param initialDateInMillis - Initial Date in Milliseconds
+ * @param minimumDateInMillis - Minimum Date in Milliseconds
+ * @param pickerStyle - Date Picker Style (dateOnly, dateAndTime)
+ * @param callback - Callback function
+ */
 const datePicker = ({
   initialDateInMillis,
   minimumDateInMillis,
   pickerStyle,
   callback,
 }) => {
-  switch (pickerStyle) {
-    case "dateOnly":
-      showMode({
-        minimumDate: minimumDateInMillis ? new Date(minimumDateInMillis) : null,
-        currentMode: "date",
-        selectedDate: new Date(initialDateInMillis),
-        callback: (date) => callback(new Date(date).getTime()),
-      });
-      break;
-    case "dateAndTime":
-      showMode({
-        minimumDate: minimumDateInMillis ? new Date(minimumDateInMillis) : null,
-        currentMode: "date",
-        selectedDate: new Date(initialDateInMillis),
-        callback: (date) =>
+  switch (Platform.OS) {
+    case "android":
+      switch (pickerStyle) {
+        case "dateOnly":
           showMode({
             minimumDate: minimumDateInMillis
               ? new Date(minimumDateInMillis)
               : null,
-            currentMode: "time",
-            selectedDate: date,
-            callback: (dateWithTime) =>
-              callback(new Date(dateWithTime).getTime()),
-          }),
-      });
+            currentMode: "date",
+            selectedDate: new Date(initialDateInMillis),
+            callback: (date) => callback(new Date(date).getTime()),
+          });
+          break;
+        case "dateAndTime":
+          showMode({
+            minimumDate: minimumDateInMillis
+              ? new Date(minimumDateInMillis)
+              : null,
+            currentMode: "date",
+            selectedDate: new Date(initialDateInMillis),
+            callback: (date) =>
+              showMode({
+                minimumDate: minimumDateInMillis
+                  ? new Date(minimumDateInMillis)
+                  : null,
+                currentMode: "time",
+                selectedDate: date,
+                callback: (dateWithTime) =>
+                  callback(new Date(dateWithTime).getTime()),
+              }),
+          });
+          break;
 
-    default:
+        default:
+          break;
+      }
       break;
   }
 };
