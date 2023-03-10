d508593d19c240015e7cfc9867ef4c9910b58e62
mhazizk <mhazizk@gmail.com>
2023-03-10 23:53:14 +0700

    Update getReportSectionProps.js

--- src/features/reports/model/getReportSectionProps.js
+++ src/features/reports/model/getReportSectionProps.js
@@ -3,39 +3,93 @@ import * as utils from "../../../utils";
 /**
  * Get section props for the report
  *
- * @param selectedMode - `monthly` or `yearly`. Default is `monthly`
+ * @param selectedMode - `thisMonth`, `monthly`, or `yearly`. Default is `monthly`
  * @returns sectionProps
  */
 const getReportSectionProps = (selectedMode = "monthly") => {
   const sections = [];
 
-  switch (selectedMode) {
-    case "monthly":
-      const thisYear = new Date().getFullYear();
-      const thisMonth = new Date().getMonth();
+  // const firstDateInThisMonth = utils.getFirstDateOfTheMonth(todayInMillis);
+  const todayInMillis = Date.now();
+  const thisYear = new Date().getFullYear();
+  const thisMonth = new Date().getMonth();
+  let year = thisYear;
+  let month = thisMonth;
 
+  const section = {
+    title: null,
+    customDate: null,
+    startDateInMillis: null,
+    endDateInMillis: null,
+    data: {
+      totalIncome: 0,
+      totalExpense: 0,
+      expenseList: [],
+      incomeList: [],
+      incomeGraph: [],
+      expenseGraph: [],
+    },
+  };
+
+  switch (selectedMode) {
+    case "thisMonth":
+      const newSection = {
+        ...section,
+        title: utils.upperCaseThisFirstLetter(
+          new Date(thisYear, thisMonth, 1).toLocaleString("default", {
+            month: "long",
+          })
+        ),
+        customDate: utils.getCustomDate(
+          new Date(thisYear, thisMonth, 1).getTime()
+        ),
+        startDateInMillis: new Date(
+          new Date(thisYear, thisMonth, 1).setHours(0, 0, 0, 0)
+        ).getTime(),
+        endDateInMillis: utils.getNextDateInRange(
+          new Date(thisYear, thisMonth, 1).getTime(),
+          "monthly"
+        ),
+        data: {
+          totalIncome: 0,
+          totalExpense: 0,
+          expenseList: [],
+          incomeList: [],
+          incomeGraph: [],
+          expenseGraph: [],
+        },
+      };
+
+      sections.push(newSection);
+
+      break;
+
+    case "monthly":
+      let totalPreviousDays = 0;
       //   repeat to get 12 months earlier
       for (let i = 0; i < 12; i++) {
-        let year = thisYear;
-        let month = thisMonth;
-        // let nextYear = thisYear + 1;
-        // let nextMonth = thisMonth + 1;
+        const relativeTimeInMillis = totalPreviousDays * 24 * 60 * 60 * 1000;
 
-        year = month < 0 ? year - i : year;
-        month = month < 0 ? 11 : month - i;
+        const timeAtTheMoment = todayInMillis - relativeTimeInMillis;
 
-        const section = {
+        const yearAtTheMoment = new Date(timeAtTheMoment).getFullYear();
+        const monthAtTheMoment = new Date(timeAtTheMoment).getMonth();
+        const dateAtTheMoment = new Date(timeAtTheMoment).getDate();
+        const firsDateAtTheMomentInMillis = new Date(
+          new Date(yearAtTheMoment, monthAtTheMoment, 1).setHours(0, 0, 0, 0)
+        ).getTime();
+
+        const newSection = {
+          ...section,
           title: utils.upperCaseThisFirstLetter(
-            new Date(year, month, 1).toLocaleString("default", {
+            new Date(timeAtTheMoment).toLocaleString("default", {
               month: "long",
             })
           ),
-          customDate: utils.getCustomDate(new Date(year, month, 1).getTime()),
-          startDateInMillis: new Date(
-            new Date(year, month, 1).setHours(0, 0, 0, 0)
-          ).getTime(),
+          customDate: utils.getCustomDate(timeAtTheMoment),
+          startDateInMillis: firsDateAtTheMomentInMillis,
           endDateInMillis: utils.getNextDateInRange(
-            new Date(year, month, 1).getTime(),
+            firsDateAtTheMomentInMillis,
             "monthly"
           ),
           data: {
@@ -48,7 +102,11 @@ const getReportSectionProps = (selectedMode = "monthly") => {
           },
         };
 
-        sections.push(section);
+        sections.push(newSection);
+        totalPreviousDays += utils.getTotalDaysInMonth(
+          yearAtTheMoment,
+          monthAtTheMoment - i
+        );
       }
 
       break;
@@ -56,7 +114,7 @@ const getReportSectionProps = (selectedMode = "monthly") => {
     default:
       break;
   }
-
+  console.log(JSON.stringify(sections, null, 2));
   return sections.sort((a, b) => a.customDate.localeCompare(b.customDate));
 };
 export default getReportSectionProps;
