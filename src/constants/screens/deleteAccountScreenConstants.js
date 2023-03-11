8fef2f8d74b5313d3a33e4319c5a816af64324e7
mhazizk <mhazizk@gmail.com>
2023-03-12 01:09:33 +0700

    Create deleteAccountScreenConstants.js

---
+++ src/constants/screens/deleteAccountScreenConstants.js
@@ -0,0 +1,14 @@
+const DELETE_ACCOUNT_SCREEN_CONSTANTS = {
+  FIRST_LABEL: "Please note that this action is irreversible.",
+  SECOND_LABEL:
+    "Deleting account means deleting all of your data, including your logbooks, categories, budgets, transactions, and any other data that you have stored in Juta.",
+  THIRD_LABEL:
+    "Your account will be deleted after 30 days since the deletion request is made.",
+  FOURTH_LABEL:
+    "During this period, you can still cancel your request by logging in to your account and coming back to this menu.",
+  FIFTH_LABEL: "Please enter your password below to confirm your action.",
+  CANCEL_BUTTON_LABEL: "Cancel",
+  DELETE_BUTTON_LABEL: "Request Deletion",
+};
+
+export default DELETE_ACCOUNT_SCREEN_CONSTANTS;
