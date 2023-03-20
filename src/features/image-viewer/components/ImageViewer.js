75aa9f32f8486dba4accc96aa21f72e406ecc88a
mhazizk <mhazizk@gmail.com>
2023-03-20 21:33:25 +0700

    Update ImageViewer.js

--- src/features/image-viewer/components/ImageViewer.js
+++ src/features/image-viewer/components/ImageViewer.js
@@ -1,59 +1,70 @@
 import { useState } from "react";
-import { Image, TouchableNativeFeedback, View } from "react-native";
+import {
+  Image,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Loading from "../../../components/Loading";
 
 const ImageViewer = ({ uri, onPress }) => {
   const [isloading, setIsLoading] = useState(true);
   return (
     <>
-      <TouchableNativeFeedback
-        onPress={() => {
-          onPress(uri);
-        }}
+      <View
         style={{
           width: 200,
           height: 200,
+          margin: 8,
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <View
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-            //   padding: 8,
+        <TouchableOpacity
+          onPress={() => {
+            onPress(uri);
           }}
         >
-          <Image
-            source={{ uri }}
-            onLoadStart={() => {
-              setIsLoading(true);
-            }}
-            onLoadEnd={() => {
-              setIsLoading(false);
-            }}
-            style={{
-              margin: 8,
-              alignSelf: "center",
-              borderRadius: 8,
-              width: 200,
-              height: 200,
-            }}
-          />
-        </View>
-      </TouchableNativeFeedback>
-      {isloading && (
-        <>
           <View
             style={{
-              // backgroundColor: "black",
-              position: "absolute",
-              top: 100,
-              left: 100,
+              alignItems: "center",
+              justifyContent: "center",
+              //   padding: 8,
             }}
           >
-            <Loading size={24} />
+            <Image
+              source={{ uri }}
+              onLoadStart={() => {
+                setIsLoading(true);
+              }}
+              onLoadEnd={() => {
+                setIsLoading(false);
+              }}
+              style={{
+                alignSelf: "center",
+                borderRadius: 8,
+                width: 200,
+                height: 200,
+              }}
+            />
           </View>
-        </>
-      )}
+        </TouchableOpacity>
+        {isloading && (
+          <>
+            <View
+              style={{
+                position: "absolute",
+                height: 100,
+                width: 100,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Loading size={24} />
+            </View>
+          </>
+        )}
+      </View>
     </>
   );
 };
