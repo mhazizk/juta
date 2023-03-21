dedfc97c9a8128847e1f35cd6c1fe6548e49b108
mhazizk <mhazizk@gmail.com>
2023-03-21 16:50:51 +0700

    Update newFeatureBadgeStartDateConstantsInMillis.js

--- src/constants/newFeatureBadgeStartDateConstantsInMillis.js
+++ src/constants/newFeatureBadgeStartDateConstantsInMillis.js
@@ -1,5 +1,5 @@
 const NEW_FEATURE_BADGE_START_DATE_CONSTANTS_IN_MILLIS = {
-  MY_REPORTS: new Date(2023, 2, 12).getTime(),
+  MY_REPORTS: new Date(2023, 2, 22).getTime(),
 };
 
 export default NEW_FEATURE_BADGE_START_DATE_CONSTANTS_IN_MILLIS;
