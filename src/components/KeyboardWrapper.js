2e365ac232d3edac6a010c772d20b88df28ad7ac
mhazizk <mhazizk@gmail.com>
2023-03-25 09:48:51 +0700

    Update KeyboardWrapper.js

--- src/components/KeyboardWrapper.js
+++ src/components/KeyboardWrapper.js
@@ -2,19 +2,23 @@ import { KeyboardAvoidingView, Platform } from "react-native";
 import { useGlobalTheme } from "../reducers/GlobalContext";
 import { useHeaderHeight } from "@react-navigation/elements";
 import Constants from "expo-constants";
+import { useEffect } from "react";
 
-const KeyboardViewWrapper = ({ isTextInput, children }) => {
+const KeyboardViewWrapper = ({ isTextInput = false, children }) => {
   const { globalTheme } = useGlobalTheme();
   const headerHeight = useHeaderHeight();
-  const keyboardVerticalOffset =
+  let offset =
     Platform.OS === "android" ? headerHeight + Constants.statusBarHeight : 0;
+
   return (
     <>
       {isTextInput && (
         <KeyboardAvoidingView
           behavior="padding"
-          keyboardVerticalOffset={keyboardVerticalOffset}
+          keyboardVerticalOffset={offset}
           style={{
+            // width: "100%",
+            // alignItems: "center",
             backgroundColor: globalTheme.colors.background,
             borderTopRightRadius: 16,
             borderTopLeftRadius: 16,
