b9a76712b3bf269613b9443c9b815e290f0aa1f2
mhazizk <mhazizk@gmail.com>
2023-03-27 22:21:08 +0700

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -3,14 +3,14 @@ module.exports = ({ config }) => {
   const expoProjectID = "4c20a6fe-d8c0-40a2-9324-2f39af56f8a3";
   const channelNameOptions = ["production", "staging", "development"];
 
-  const appVersion = "0.9.2";
-  const androidVersionCode = 31;
+  const appVersion = "0.10.0";
+  const androidVersionCode = 32;
 
   // Follow apple guidelines for build number e.g. "1.0.0" with no additional characters
   // For eas submit, the Transporter will get the version from eas.version
   // So to make it safe, we will use the same version for build number
 
-  const iosBuildNumber = "0.8.7";
+  const iosBuildNumber = "0.10.0";
 
   const channelName = channelNameOptions[0];
 
