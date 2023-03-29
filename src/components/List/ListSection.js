167c4877abf927b7eac24a522522b75a8dd01560
mhazizk <mhazizk@gmail.com>
2023-03-30 01:06:43 +0700

    Update ListSection.js

--- src/components/List/ListSection.js
+++ src/components/List/ListSection.js
@@ -17,6 +17,7 @@ const ListSection = ({
   backgroundColor = null,
   noBackgroundColor = false,
   backgroundOpacity = 0.07,
+  keyboardTranslation = null,
 }) => {
   const { appSettings } = useGlobalAppSettings();
   const { globalTheme } = useGlobalTheme();
@@ -24,31 +25,54 @@ const ListSection = ({
     <>
       <Animated.View
         entering={enteringAnimation}
-        style={{
-          width: "100%",
-          marginTop: marginTop ? marginTop : 0,
-          marginBottom: noMargin ? 0 : marginBottom,
-          overflow: "hidden",
-          borderWidth: borderOnly ? 3 : 0,
-          borderColor: borderColor
-            ? borderColor
-            : utils.hexToRgb({
-                hex: borderColor || globalTheme.colors.listSection,
-                opacity: backgroundOpacity,
-              }),
-          borderRadius: 16,
-          marginHorizontal: 16,
-          backgroundColor: noBackgroundColor
-            ? "transparent"
-            : backgroundOpacity
-            ? utils.hexToRgb({
-                hex: backgroundColor || globalTheme.colors.listSection,
-                opacity: backgroundOpacity,
-              })
-            : backgroundColor,
-        }}
+        style={[
+          {
+            zIndex: 1,
+            width: "100%",
+            marginTop: marginTop ? marginTop : 0,
+            marginBottom: noMargin ? 0 : marginBottom,
+            overflow: "hidden",
+            borderWidth: borderOnly ? 3 : 0,
+            borderColor: borderColor
+              ? borderColor
+              : utils.hexToRgb({
+                  hex: borderColor || globalTheme.colors.listSection,
+                  opacity: backgroundOpacity,
+                }),
+            borderRadius: 16,
+            marginHorizontal: 16,
+          },
+          {
+            ...keyboardTranslation,
+          },
+        ]}
       >
-        {children}
+        <Animated.View
+          style={{
+            zIndex: 2,
+            backgroundColor: noBackgroundColor
+              ? "transparent"
+              : backgroundOpacity
+              ? utils.hexToRgb({
+                  hex: backgroundColor || globalTheme.colors.listSection,
+                  opacity: backgroundOpacity,
+                })
+              : backgroundColor,
+          }}
+        >
+          {children}
+        </Animated.View>
+        {keyboardTranslation && (
+          <View
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              zIndex: 0,
+              backgroundColor: globalTheme.colors.background,
+            }}
+          />
+        )}
       </Animated.View>
     </>
   );
